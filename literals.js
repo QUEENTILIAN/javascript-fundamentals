dfecfab0f9621824be17757983bc93f8867874a0
QUEENTILIAN <queentilianatieno@gmail.com>
2022-10-15 16:43:42 +0300

    first commit

---
+++ literals.js
@@ -0,0 +1,18 @@
+
+console.log(`I counted ${5+6} goats`);
+let firstName = 'Queenty';
+let lastName = 'Atieno';
+// let fullName =firstName + '  ' + lastName;
+// console.log(fullName);
+let fullName = ` ${firstName} ${lastName}`;
+console.log(fullName);
+let totalOne =500;
+let totalTwo =800;
+let totals =`kshs. ${totalOne + totalTwo}`;
+console.log(totals);
+let item ='cucumber';
+let price = 1.99;
+let quantity = 4;
+` You bought ${quantity} ${item}, total price: $${price*quantity}`;
+console.log(totalprice);
+
---
+++ strings.js
@@ -0,0 +1,14 @@
+ let annoyinglaugh='teehee so funny! teehee!';
+annoyinglaugh.replace('teehee', 'haha');
+ console.log(annoyinglaugh);
+ console.log(annoyinglaugh.replace('teehee', 'haha'));
+ const age ="5" +"4";
+ console.log(age);
+ console.log("pecan pie"[7]);
+ console.log("pup"[3]);
+ console.log(annoyinglaugh.replaceAll('teehee', 'haha'));
+// new line
+console.log( 'hello\nworld\n kenya');
+console.log( 'hello \'world\'');
+console.log( 'hello \\'world'');
+console.log( 'hello\nworld\r kenya');
