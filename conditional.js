cc8376d785f8c54c69e79acde35eb2b9110d4df4
QUEENTILIAN <queentilianatieno@gmail.com>
2022-10-20 15:24:36 +0300

    first commit

---
+++ conditional.js
@@ -0,0 +1,54 @@
+//COMPARISON OPERATORS
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+console.log(10!== `10`);
+console.log(`i am a template literal but to embade me use ${120}`);
+let rating = 2;
+if(rating === 3) {console.log("YOU ARE A SUPERSTAR!");}
+if(rating === 2) {console.log("YOU ARE A SUPERSTAR!");}
+else if (rating === 2) {
+    console.log("MEETS EXPECTATIONS");}
+    // let days = 7
+    // if(days === 7) {console.log("saturday");}
+    // else if (days === 6) {console.log("friday");}
+    // if(days === 1) {console.log("sunday");}
+    // else if (days === 2) {console.log("monday");}
+    // if(days === 3) {console.log("tuesday");}
+    // else if (days === 4) {console.log("wednesday");} 
+    //create a varible with the name password.and give any value
+    //THE VALUE OF THE PASSWORD MUST BE A STRING
+    //VALUE OF THE PASSWORD MUST BE GREATER THAN OR EQUALS TO SIX CHARACTERS
+    //ENSURE THE VALUE OF THE PASSWORD HAS NO SPACES BETWEEN
+    let age = 20;
+   console.log( age === 20? 'correct age' : 'Wrong age');
+   //switch statements
+   let days = 7
+   switch (days) {
+    case 1:
+        console.log ('monday');
+          break;
+    case 2:
+        console.log('tuesday');
+         break;
+    case 3:
+        console.log('wednesday');
+         break;
+    case 4:
+        console.log('thursday');
+         break;
+    case 5:
+        console.log('friday');
+        break;
+    case 6: 
+    console.log('saturday');
+        break;
+
+    
+   }
+
+    
+
+
+
